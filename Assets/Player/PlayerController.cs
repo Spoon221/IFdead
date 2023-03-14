7cc66184d7c8da2eed5d88383bd6df97485b2fdc
Artyom Sokolov <15012003artem@mail.ru>
2023-03-14 20:55:24 +0500

    сделал основы поворота камеры

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -9,16 +9,17 @@ public class PlayerController : MonoBehaviour
     private const float correctorIateralSpeed = 14f;
     private const float correctorFrontSpeed = 18f;
 
+    [SerializeField] private Transform camera;
+
     [field: SerializeField] public int MaxHealth { get; private set; }
     private int currentHealth;
     [field: SerializeField] public float MaxMana { get; private set; }
     private float currentMana;
 
     private Rigidbody physicsPlayer;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
-    private float axisX;
-    private float axisZ;
-    
     public UnityEvent<int> OnHealthChanged = new UnityEvent<int>();
     public UnityEvent<float> OnManaChanged = new UnityEvent<float>();
 
@@ -40,13 +41,22 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        axisX = Input.GetAxis("HorizontalX");
-        axisZ = Input.GetAxis("HorizontalZ");
+        var axisX = Input.GetAxis("HorizontalX");
+        var axisZ = Input.GetAxis("HorizontalZ");
+        var direction = new Vector3(axisX, 0f, axisZ).normalized;
+        var moveDirection = Vector3.zero; 
+
+        if (direction.magnitude >= 0.1f)
+        {
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        }
 
         if (physicsPlayer.velocity.magnitude < maxSpeed)
-        {
-            physicsPlayer.AddRelativeForce(axisX * correctorIateralSpeed, 0f, axisZ * correctorFrontSpeed);
-        }
+            physicsPlayer.AddRelativeForce(moveDirection.normalized * correctorFrontSpeed);
     }
 
     private void RestoreHealth(int amountOfHealth)
