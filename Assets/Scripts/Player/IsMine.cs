6040aa0edee01e4fd778740d5360bf5297af0e52
Merczz <simakov.jenja@gmail.com>
2023-04-17 18:49:47 +0500

    еще фикс

--- Assets/Scripts/Player/IsMine.cs
+++ Assets/Scripts/Player/IsMine.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class class1 : MonoBehaviour
+public class IsMine : MonoBehaviour
 {
     public PhotonView view;
     public PlayerMovementController scripsPlayerMovementController;
--- Assets/Scripts/Player/ThirdPersonCameraController.cs
+++ Assets/Scripts/Player/ThirdPersonCameraController.cs
@@ -16,6 +16,8 @@ public class ThirdPersonCameraController : MonoBehaviour
     public float maxXSensitivity;
     public float maxYSensitivity;
 
+    public PhotonView view;
+
     private void Start()
     {
         cinemachineVirtualCamera = gameObject.GetComponent<CinemachineFreeLook>();
@@ -26,15 +28,18 @@ public class ThirdPersonCameraController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
+        if (view.IsMine)
+        {
+            Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+            orientation.forward = viewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        if (inputDir != Vector3.zero)
-            playerModel.forward =
-                Vector3.Slerp(playerModel.forward, inputDir.normalized, Time.deltaTime * rotationModelSpeed);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            if (inputDir != Vector3.zero)
+                playerModel.forward =
+                    Vector3.Slerp(playerModel.forward, inputDir.normalized, Time.deltaTime * rotationModelSpeed);
+        }
     }
 
     private void ChangeSensitivity(float sensitivity)
