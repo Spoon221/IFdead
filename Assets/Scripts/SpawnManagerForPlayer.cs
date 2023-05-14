066b81f2edd0e1e203ddc8173fe1987f90ec214f
Merczz <simakov.jenja@gmail.com>
2023-05-14 19:56:44 +0500

    еще один чуть-чуть не кривой билд

--- Assets/Scripts/SpawnManagerForPlayer.cs
+++ Assets/Scripts/SpawnManagerForPlayer.cs
@@ -7,12 +7,19 @@ public class SpawnManagerForPlayer : MonoBehaviour
 {
     public GameObject[] Spawns;
     public GameObject Player;
+    public GameObject Maniac;
 
 
     public void Start()
     {
         var randomPositions = Spawns[Random.Range(0, Spawns.Length)].transform.localPosition;
-
-        PhotonNetwork.Instantiate(Player.name, randomPositions, Quaternion.identity);
+        if (PhotonNetwork.PlayerList.Length == 1)
+        {
+            PhotonNetwork.Instantiate(Maniac.name, randomPositions, Quaternion.identity);
+        }
+        else if (PhotonNetwork.PlayerList.Length > 1)
+        {
+            PhotonNetwork.Instantiate(Player.name, randomPositions, Quaternion.identity);
+        }
     }
 }
