f5a27b9d995ce2c977cc478588d69279d4fa0da5
iAtmos <azeev.b01@gmail.com>
2023-06-02 10:35:05 +0500

    1

--- Assets/Scripts/Character/Bot/FieldOfViewEditor.cs
+++ Assets/Scripts/Character/Bot/FieldOfViewEditor.cs
@@ -1,33 +1,33 @@
-using UnityEditor;
-using UnityEngine;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(AI_FieldOfView))]
-public class FieldOfViewEditor : Editor
-{
-    private void OnSceneGUI()
-    {
-        var fov = (AI_FieldOfView)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.RadiusFieldsView);
+//[CustomEditor(typeof(AI_FieldOfView))]
+//public class FieldOfViewEditor : Editor
+//{
+//    private void OnSceneGUI()
+//    {
+//        var fov = (AI_FieldOfView)target;
+//        Handles.color = Color.white;
+//        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.RadiusFieldsView);
 
-        Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.AngleView / 2);
-        Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.AngleView / 2);
+//        Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.AngleView / 2);
+//        Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.AngleView / 2);
 
-        Handles.color = Color.yellow;
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.RadiusFieldsView);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.RadiusFieldsView);
+//        Handles.color = Color.yellow;
+//        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.RadiusFieldsView);
+//        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.RadiusFieldsView);
 
-        if (fov.canSeePlayer)
-        {
-            Handles.color = Color.green;
-            //Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
-        }
-    }
+//        if (fov.canSeePlayer)
+//        {
+//            Handles.color = Color.green;
+//            //Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+//        }
+//    }
 
-    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
-    {
-        angleInDegrees += eulerY;
+//    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+//    {
+//        angleInDegrees += eulerY;
 
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
-    }
-}
+//        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+//    }
+//}
