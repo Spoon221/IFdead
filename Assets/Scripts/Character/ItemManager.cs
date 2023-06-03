62982d93c8231262306d65cf61d05bfbce0006f6
Artyom Sokolov <15012003artem@mail.ru>
2023-06-03 16:03:22 +0500

    Добавил всплывающее окошко при приближении к интерактивному предмету

--- Assets/Scripts/Character/ItemManager.cs
+++ Assets/Scripts/Character/ItemManager.cs
@@ -2,28 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ItemManager : MonoBehaviour
 {
+    [FormerlySerializedAs("ItemMessage")] [SerializeField]
+    private GameObject itemMessage;
+
+    [SerializeField] private TMP_Text actionText;
+
+    public void Start()
+    {
+        itemMessage.SetActive(false);
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.TryGetComponent(out PickableItem pickableItem))
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (!pickableItem.IsCollected)
             {
-                if (!pickableItem.IsCollected)
+                actionText.text = $"Подобрать {pickableItem.ItemName}";
+                itemMessage.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
                     pickableItem.PickUpItem();
+                    itemMessage.SetActive(false);
+                }
             }
         }
 
         else if (other.TryGetComponent(out ActivatedItem item))
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (!item.IsActivated)
             {
-                if (!item.IsActivated)
+                actionText.text = item.ItemName;
+                itemMessage.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
                     item.ActivateItem();
+                    itemMessage.SetActive(false);
+                }
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out PickableItem pickableItem) || other.TryGetComponent(out ActivatedItem item))
+            itemMessage.SetActive(false);
     }
 }
