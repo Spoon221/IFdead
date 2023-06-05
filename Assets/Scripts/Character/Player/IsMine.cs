449190a554fe50002570271347dcc73881f3c31f
Merczz <simakov.jenja@gmail.com>
2023-06-05 08:42:00 +0500

    фикс анимации выжившего

--- Assets/Scripts/Character/Player/IsMine.cs
+++ Assets/Scripts/Character/Player/IsMine.cs
@@ -1,24 +1,20 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class IsMine : MonoBehaviour
+public class IsMine : MonoBehaviourPun
 {
-    public PhotonView view;
     public PlayerMovementController scriptPlayerMovementController;
     public ManiacMovementController scriptManiacMovementController;
     public ItemManager scriptPlayerItemManager;
     public Canvas Bar;
-    public Canvas eCode;
     public Camera camera;
 
     private void Awake()
     {
-        view = GetComponent<PhotonView>();
-        if (!view.IsMine)
+        if (!photonView.IsMine)
         {
             camera.enabled = false;
             Bar.enabled = false;
-            eCode.enabled = false;
             scriptPlayerMovementController.enabled = false;
             scriptPlayerItemManager.enabled = false;
             scriptManiacMovementController.enabled = false;
