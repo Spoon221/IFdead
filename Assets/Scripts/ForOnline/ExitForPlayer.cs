0b424a31311d780580257b1b14fe7cd5ebb9497d
SpOOn <simakov.jenja@gmail.com>
2023-11-28 15:05:35 +0300

    добавлена возможность побега с локации по завершении заданий

--- Assets/Scripts/ForOnline/ExitForPlayer.cs
+++ Assets/Scripts/ForOnline/ExitForPlayer.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 
 public class ExitForPlayer : MonoBehaviour
 {
     [SerializeField] private Settings settings;
-    [SerializeField] private PhotonView view;
     [SerializeField] private Generator generator;
+    [SerializeField] private GameObject WinnerCanvas;
     private MeshRenderer meshRenderer;
     private BoxCollider boxCollider;
     private int complitedTask;
+    private float displayTime = 5f;
 
     private void Start()
     {
+        WinnerCanvas.SetActive(false);
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.enabled = false;
         boxCollider = GetComponent<BoxCollider>();
@@ -28,14 +31,40 @@ public class ExitForPlayer : MonoBehaviour
             boxCollider.isTrigger = true;
             meshRenderer.enabled = true;
         }
-        var countPlayers = GameObject.FindGameObjectsWithTag("Player").Length;
-        if (countPlayers == 0)
-            settings.view.RPC("LeaveGame", RpcTarget.All);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-            settings.LeaveGame();
+        if (settings.photonView.IsMine)
+        {
+            if (other.CompareTag("Player"))
+                StartCoroutine(ShowCanvasAndLeaveGame());
+        }
+        else
+        {
+            StartCoroutine(CheckPlayersAfterCoroutine());
+        }
+        //var countPlayers = GameObject.FindGameObjectsWithTag("Player").Length;
+        //if (countPlayers < 1)
+        //    settings.view.RPC("LeaveGame", RpcTarget.All);
+    }
+
+    private IEnumerator ShowCanvasAndLeaveGame()
+    {
+        WinnerCanvas.SetActive(true);
+        yield return new WaitForSeconds(displayTime);
+        settings.LeaveGame();
+        WinnerCanvas.SetActive(false);
+
+        StartCoroutine(CheckPlayersAfterCoroutine());
+    }
+
+    private IEnumerator CheckPlayersAfterCoroutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+        var countPlayers = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (countPlayers < 1)
+            settings.view.RPC("LeaveGame", RpcTarget.All);
     }
 }
