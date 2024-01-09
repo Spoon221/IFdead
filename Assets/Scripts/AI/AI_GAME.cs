96a38d20368eb391b88bf65fe7e5a671d485d5dd
likip3 <likip3@gmail.com>
2024-01-09 15:08:46 +0500

    singleplayer

--- Assets/Scripts/AI/AI_GAME.cs
+++ Assets/Scripts/AI/AI_GAME.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class AI_GAME
 {
     public static bool ExitOpened;
 
+    public static Transform[] generators;
+    public static Transform[] exits;
 
     static AI_GAME() => StartGame();
 
     public static void StartGame()
     {
         ExitOpened = false;
+        generators = Array.ConvertAll(GameObject.FindObjectsOfType<Generator>(), item => item.transform);
+        exits = Array.ConvertAll(GameObject.FindObjectsOfType<ExitForPlayer>(), item => item.transform);
+
     }
 }
--- Assets/Scripts/AI/AI_Man.cs
+++ Assets/Scripts/AI/AI_Man.cs
@@ -1,31 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class AI_Man : MonoBehaviour
 {
     private NavMeshAgent agent;
     public LayerMask layerMask;
-    public Transform surv;
+    public Transform[] SurvPlayers = {};
+
+
+    private int timer;
+
+    private Transform[] generators => AI_GAME.generators;
+    private Transform[] exits => AI_GAME.exits;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        SurvPlayers = Array.ConvertAll(GameObject.FindGameObjectsWithTag("Survival"), item => item.transform);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        agent.SetDestination(surv.position);
+        var surv = SurvPlayers[FindNearestTrans(SurvPlayers)];
+        if (CanSeeSurv(surv) || timer > 0)
+        {
+            agent.SetDestination(surv.position);
+            timer--;
+        }
+        else
+        {
+            WalkToNearest_Generator();
+        }
     }
 
-    private bool CanSeeSurv()
+    private bool CanSeeSurv(Transform surv)
     {
+        
         if (Vector3.Distance(surv.position, transform.position) > 20) return false;
 
-        Ray ray = new(transform.position + new Vector3(0, 2, 0), surv.position - new Vector3(0, 1, 0) - transform.position);
-        Debug.DrawRay(transform.position + new Vector3(0, 2, 0), surv.position - new Vector3(0, 1, 0) - transform.position);
+        Ray ray = new(transform.position + new Vector3(0, 1, 0), surv.position - new Vector3(0, 1, 0) - transform.position);
+        Debug.DrawRay(transform.position + new Vector3(0, 1, 0), surv.position - new Vector3(0, 1, 0) - transform.position, Color.red);
         Physics.Raycast(ray, out var hit, layerMask);
-        return hit.collider.tag == "Survival";
+        if (hit.collider == null) return false;
+
+        if (hit.collider?.tag != "Survival") return false;
+        timer = 5 * 40;
+        return true;
+
+    }
+
+    private int nowGen;
+    public void WalkToNearest_Generator()
+    {
+        //agent.stoppingDistance = 2;
+        if(nowGen ==-1)
+            nowGen = FindNearestTrans(generators);
+        if(Vector3.Distance(transform.position, generators[nowGen].position) < 2)
+            nowGen = Random.Range(0,generators.Length);
+        agent.SetDestination(generators[nowGen].position);
+        agent.isStopped = false;
+    }
+
+    public int FindNearestTrans(Transform[] points)
+    {
+        var nearest = 0;
+
+        for (var i = 1; i < points.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, points[i].position) < Vector3.Distance(transform.position, points[nearest].position))
+            {
+                nearest = i;
+            }
+        }
+
+        return nearest;
     }
 }
